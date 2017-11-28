9cd550c58d85c1435301b700129ab5e559ef33af
emanuelibiapino <31495741+emanuelibiapino@users.noreply.github.com>
2017-11-28 17:08:46 -0300

    upload do projeto de web I
    Fis o upload de todos os arquivos relevantes do projeto final de programação web I

---
+++ cookie.js
@@ -0,0 +1,43 @@
+/*function writeCookie(name, value, days){
+	var expires = "";
+
+	if(days){
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		expires = "; expires=" + date.toGMTString();
+	}
+
+	if(value!="" && value!=NULL && value!="null"){
+		document.cookie = name + "=" + value + expires + "; path=/";
+	}
+
+}
+
+function readCookie(name){
+	var searchName = name + "=";
+	var cookies = document.cookie.split(';');
+	for(var i = 0; i<cookies.length; i++){
+		var c = cookies[i];
+		while(c.charAt(0) == ' '){
+			c = c.substring(1, c.length);
+		}
+		if(c.indexOf(searchname) == 0){
+			return c.substring(searchName.length, c.length);
+		}
+	}
+	return null;
+}
+
+function eraseCookie(name){
+	writeCookie(name, "", -1);
+}*/
+
+function criarCookie(valorCookie){
+	var data = new Date();
+
+	data.setTime(data.getTime() + 60000);
+	document.cookie = "primeiroCookie=" + valorCookie +"; expires=" + data.toGMTString() + "; path=/";
+
+	return "Sucesso";
+}
+
---
+++ home.js
@@ -0,0 +1,49 @@
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+        alert("Bem Vindo " + user);
+    } else {
+       user = prompt("Digite seu nome:","");
+       if (user != "" && user != null) {
+           setCookie("username", user, 30);
+       }
+    }
+}
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('a[href="#top"]').fadeIn();
+        } else {
+            $('a[href="#top"]').fadeOut();
+        }
+    });
+
+    $('a[href="#top"]').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+});
